b535b5dd8033eceb62cd2f6ee1928d7940f11d45
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-10-10 09:52:20 -0500

    notes and review

--- albums_migration.sql
+++ albums_migration.sql
@@ -4,7 +4,7 @@ CREATE TABLE albums (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     artist VARCHAR(250) NOT NULL,
     name VARCHAR(100) NOT NULL,
-    release_date INT(4) UNSIGNED,
+    release_date INT(4)  UNSIGNED,
     sales FLOAT (5,2) UNSIGNED,
     genre VARCHAR(255),
     PRIMARY KEY (id)
