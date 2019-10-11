a1f19cff8f7fd5ebc2aebf844f7c503d9800d10d
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-10-11 13:48:11 -0500

    updated during walkthrough

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,47 +1,57 @@
 USE employees;
-
-SELECT emp_no, first_name, last_name
+# Create a file named where_exercises.sql. Make sure to use the employees database.
+# Find all employees with first names 'Irena', 'Vidya', or 'Maya' — 709 rows (Hint: Use IN).
+SELECT *
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 
-SELECT emp_no, first_name, last_name
+# Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT *
 FROM employees
 WHERE last_name like ('E%');
 
-SELECT emp_no, first_name, last_name, hire_date
+# Find all employees hired in the 90s — 135,214 rows.
+SELECT *
 FROM employees
 WHERE hire_date Like '199%';
 
+# Find all employees born on Christmas — 842 rows.
 SELECT emp_no, first_name, last_name, birth_date
 FROM employees
 WHERE birth_date like '%12-25%';
 
+# Find all employees with a 'q' in their last name — 1,873 rows.
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name like ('%q%');
+# --------------------------------------------------------------------------
 
+# Update your query for 'Irena', 'Vidya', or 'Maya' to use OR instead of IN — 709 rows.
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE (first_name ='Irena') OR (first_name ='Maya') OR (first_name ='Vidya');
 
+# Add a condition to the previous query to find everybody with those names who is also male — 441 rows.
 SELECT emp_no, first_name, last_name, gender
 FROM employees
 WHERE (gender = 'M' AND  first_name ='Irena') OR (gender = 'M' AND first_name ='Maya') OR (gender = 'M' AND first_name ='Vidya') ;
 
-
+# Find all employees whose last name starts or ends with 'E' — 30,723 rows.
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name like ('E%') OR (last_name like '%E');
 
+# Duplicate the previous query and update it to find all employees whose last name starts and ends with 'E' — 899 rows.
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name like ('E%') AND (last_name like '%E');
 
-
+# Find all employees hired in the 90s and born on Christmas — 362 rows.
 SELECT emp_no, first_name, last_name, hire_date, birth_date
 FROM employees
 WHERE hire_date Like '199%' AND birth_date like '%12-25%';
 
+# Find all employees with a 'q' in their last name but not 'qu' — 547 rows.
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name like ('%q%')
