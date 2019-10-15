640a65c23820fc6776a519f5768046059dc693c1
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-10-15 15:43:45 -0500

    8/15

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,12 +1,12 @@
 USE employees;
 
 # 3 Return 10 employees in a result set named 'full_name' in the format of 'last name, first name' for each employee.
-SELECT CONCAT(last_name, ' ', first_name) AS full_name
+SELECT CONCAT(last_name, ', ', first_name) AS full_name
 FROM employees
 LIMIT 10;
 
 # 4 Add the date of birth for each employee as 'DOB' to the query.
-SELECT CONCAT(last_name, ' ', first_name) AS full_name, birth_date AS DOB
+SELECT CONCAT(last_name, ', ', first_name) AS full_name, birth_date AS DOB
 FROM employees
 LIMIT 10;
 
