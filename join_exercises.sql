b17adb4178c08d92411a913b4ecececa9e819bac
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-10-16 15:10:23 -0500

    after walkthrough

--- join_exercises.sql
+++ join_exercises.sql
@@ -9,21 +9,20 @@ JOIN departments  on dept_manager.dept_no = departments.dept_no
 WHERE to_date > NOW()
 ORDER BY dept_name;
 
-
 # 3 Find the name of all departments currently managed by women.
-SELECT departments.dept_name, CONCAT(employees.first_name,' ', employees.last_name) AS MANAGER_NAME
+SELECT departments.dept_name AS DEPARTMENT_NAME, CONCAT_WS(' ', employees.first_name, employees.last_name) AS MANAGER_NAME
 FROM employees
 JOIN dept_manager  on employees.emp_no = dept_manager.emp_no
 JOIN departments  on dept_manager.dept_no = departments.dept_no
-WHERE to_date > NOW() AND gender = 'F'
+WHERE dept_manager.to_date > NOW() AND employees.gender = 'F'
 ORDER BY dept_name;
 
 # 4 Find the current titles of employees currently working in the Customer Service department.
-SELECT titles.title, count(*)
+SELECT titles.title, COUNT(*)
 FROM titles
 JOIN employees on titles.emp_no = employees.emp_no
 JOIN dept_emp  on employees.emp_no = dept_emp.emp_no
-WHERE titles.to_date > NOW() AND dept_no = 'd009'
+WHERE dept_emp.to_date > NOW() AND titles.to_date > NOW() AND dept_no = 'd009'
 group by titles.title;
 
 # 5 Find the current salary of all current managers.
@@ -34,3 +33,5 @@ JOIN departments on dept_manager.dept_no = departments.dept_no
 JOIN salaries on employees.emp_no = salaries.emp_no
 WHERE dept_manager.to_date > NOW() AND salaries.to_date > NOW()
 ORDER BY dept_name;
+
+SELECT * FROM departments;
