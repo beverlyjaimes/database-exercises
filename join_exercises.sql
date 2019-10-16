f7967533f8977fcf3672bab09b25cb7b8b8fdf8b
beverlyjaimes <beverly.jaimespuente@gmail.com>
2019-10-16 14:17:59 -0500

    joins exercise

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,5 +1,36 @@
 USE employees;
 
 # Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
+# 2 Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
+SELECT departments.dept_name, CONCAT(employees.first_name,' ', employees.last_name) AS DEPARTMENT_MANAGER
+FROM employees
+JOIN dept_manager  on employees.emp_no = dept_manager.emp_no
+JOIN departments  on dept_manager.dept_no = departments.dept_no
+WHERE to_date > NOW()
+ORDER BY dept_name;
 
-SELECT
+
+# 3 Find the name of all departments currently managed by women.
+SELECT departments.dept_name, CONCAT(employees.first_name,' ', employees.last_name) AS MANAGER_NAME
+FROM employees
+JOIN dept_manager  on employees.emp_no = dept_manager.emp_no
+JOIN departments  on dept_manager.dept_no = departments.dept_no
+WHERE to_date > NOW() AND gender = 'F'
+ORDER BY dept_name;
+
+# 4 Find the current titles of employees currently working in the Customer Service department.
+SELECT titles.title, count(*)
+FROM titles
+JOIN employees on titles.emp_no = employees.emp_no
+JOIN dept_emp  on employees.emp_no = dept_emp.emp_no
+WHERE titles.to_date > NOW() AND dept_no = 'd009'
+group by titles.title;
+
+# 5 Find the current salary of all current managers.
+SELECT departments.dept_name, CONCAT(employees.first_name,' ', employees.last_name) AS Name, salaries.salary
+FROM employees
+JOIN dept_manager on employees.emp_no = dept_manager.emp_no
+JOIN departments on dept_manager.dept_no = departments.dept_no
+JOIN salaries on employees.emp_no = salaries.emp_no
+WHERE dept_manager.to_date > NOW() AND salaries.to_date > NOW()
+ORDER BY dept_name;
